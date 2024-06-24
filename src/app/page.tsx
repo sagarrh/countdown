5faa10a248ca098e3ac11c2cb563a17bde3094d0
sagarrh <harsorasagar04@gmail.com>
2024-06-24 23:58:27 +0530

    added

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Background from "../components/background/Background";
+import { CountdownProvider } from "../hooks/CountdownContext";
+
+
+
+export default function Home() {
+  return (
+    <main className="w-full h-full flex flex-col items-center justify-center">
+        <CountdownProvider
+        // // <Background />
+        // // <Countdown />
+        // // <Settings />
+        // // <DonateButton />
+        // // <GitHubButton />
+        // // <ShareButton />
+        // <Background>
+
+      </CountdownProvider>
+    </main>
+  );
+}
