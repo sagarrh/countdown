c8f71ec9326c763ec4e4a0d410e5aeacc05ad188
sagarrh <harsorasagar04@gmail.com>
2024-06-24 23:58:55 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default function Home() {
   return (
     <main className="w-full h-full flex flex-col items-center justify-center">
         <CountdownProvider
-        // // <Background />
-        // // <Countdown />
-        // // <Settings />
-        // // <DonateButton />
-        // // <GitHubButton />
-        // // <ShareButton />
-        // <Background>
+        // <Background />
+        // <Countdown />
+        // <Settings />
+        // <DonateButton />
+        // <GitHubButton />
+        // <ShareButton />
+        <Background>
 
       </CountdownProvider>
     </main>
