f845508e7a322ba24fd9bdb8bb61d24f01a232cc
sagarrh <harsorasagar04@gmail.com>
2024-06-25 23:54:06 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,16 @@
 "use client";
 
-import Background from "../components/background/Background";
-import { CountdownProvider } from "../hooks/CountdownContext";
+import React from 'react'
 
-
-
-export default function Home() {
+const page = () => {
   return (
-    <main className="w-full h-full flex flex-col items-center justify-center">
-        <CountdownProvider
-        <Background />
-        <Countdown />
-        <Settings />
-        <DonateButton />
-        <GitHubButton />
-        <ShareButton />
-
-      </CountdownProvider>
-    </main>
-  );
+    <>
+    <span>
+        <div>done done done done    </div>
+    </span>
+    </>
+    
+  )
 }
+
+export default page
